07a0d68ed3d5082a9f0bfefdbb611bdc798a12d5
Ibrahim <764108@cognizant.com>
2019-05-01 13:49:28 +0100

    specialised classes

--- src/com/pluralsight/calcengine/Main.java
+++ src/com/pluralsight/calcengine/Main.java
@@ -34,6 +34,24 @@ public class Main {
 
 		System.out.print("Result= ");
 		System.out.println(equationOverload.getResult());
+
+		System.out.println();
+		System.out.println("Using inheritance");
+		System.out.println();
+
+		CalculateBase[] calculators = {
+				new Divider(100.0d,50.0d),
+				new Adder(25.0d,92.0d),
+				new Subtractor(225.0d,17.0d),
+				new Multiplier(11.0d,3.0d)
+		};
+
+		for (CalculateBase calculator:calculators) {
+			calculator.calculate();
+
+			System.out.print("Result= ");
+			System.out.println(calculator.getResult());
+		}
     }
 
 //    public static MathsEquation create(double leftVal, double rightVal, char opCode) {
